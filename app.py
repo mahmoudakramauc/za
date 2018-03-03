fe2d923cf7694f383b7e04e1a0140bacdeee1390
Mohamed Ashmawy <ashma010@umn.edu>
2018-03-03 00:38:10 -0500

    -Add index.html -Add routing to home page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Resource, Api, abort
 
 app = Flask(__name__)
 api = Api(app)
 
 deroos = {'active':[
-                 {'scholar_name': 'Alhabib',
+                 {'scholar_name': 'alhabib',
                   'sex': 'm',
                   'topic': 'tazkeya',
                   'location': 'https://goo.gl/maps/zj7RqYZAcqk',
@@ -45,17 +45,18 @@ deroos = {'active':[
                   }]
         }
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-class welcome(Resource):
-    def get(self):
-        return 'zakeeha API v1.0 - This is an attempt to collect information around available/active\n'\
-        'islamic sessions/seminars and serve them in a RESTful API available for consumption by anyone for free.\n\n'\
-        'The following is a list of available GET endpoints (so far):\n'\
-        '- /all_deroos (returns all available lists of deroos whether active or not\n'\
-        '- /active_deroos (returns a list of active deroos only\n'\
-        '- /inactive_deroos (return a list of inactive deroos only\n'\
-        '- /scholar_names (retruns a list of names for all available scholars\n'\
-        '- /all_deroos/<scholar_name> returns a list of all deroos by scholar_name'
+      # return 'zakeeha API v1.0 - This is an attempt to collect information around available/active\n'\
+      # 'islamic sessions/seminars and serve them in a RESTful API available for consumption by anyone for free.\n\n'\
+      # 'The following is a list of available GET endpoints (so far):\n'\
+      # '- /all_deroos (returns all available lists of deroos whether active or not\n'\
+      # '- /active_deroos (returns a list of active deroos only\n'\
+      # '- /inactive_deroos (return a list of inactive deroos only\n'\
+      # '- /scholar_names (retruns a list of names for all available scholars\n'\
+      # '- /all_deroos/<scholar_name> returns a list of all deroos by scholar_name'
 
 class all_deroos(Resource):
     def get(self):
@@ -91,7 +92,7 @@ class deroos_by_scholar_name(Resource):
         return deroos_by_scholar_name
 
 
-api.add_resource(welcome, '/')
+#api.add_resource(welcome, '/')
 api.add_resource(all_deroos, '/all_deroos')
 api.add_resource(active_deroos, '/active_deroos')
 api.add_resource(inactive_deroos, '/inactive_deroos')
