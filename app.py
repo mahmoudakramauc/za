07ba735cd9dcf4b06f2f717e0215f07108818cd4
Mahmoudakram <elsha017@umn.edu>
2018-03-22 18:50:42 -0500

    s

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ api = Api(app)
 deroos = {'active':[
                  {'scholar_name': 'alhabib',
                   'sex': 'm',
-                  'topic': 'tazkeya',
+                  'topic': 'Tazkeyah',
                   'title': 'Kabas Al Noor Al Mobeen Men Ihyaa Uloom Al Deen',
                   'location': 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29355.758025942734!2d31.246216399673987!3d30.04779539037385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzDCsDAyJzUyLjEiTiAzMcKwMTUnNDkuNCJF!5e1!3m2!1sen!2sus!4v1521598252622',
                   'schedule':
@@ -21,8 +21,8 @@ deroos = {'active':[
                   
                 { 'scholar_name': 'muhammad mehanna',
                   'sex': 'm',
-                  'topic': 'tazkeya',
-                  'title': 'shar7 ketab 27ya2 3loom al deen',
+                  'topic': 'Tazkeya',
+                  'title': 'shar7 ketab Ihyaa Uloom Al Deen',
                   'location': 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3669.5478245093313!2d31.260496415259176!3d30.04568798188196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145840a2f3fd21f5%3A0x676752c74b1e52e8!2sAl-Azhar+Mosque!5e1!3m2!1sen!2sus!4v1521598500037',
                   'schedule':
                                 {'weekly_or_monthly': 'm',
@@ -34,8 +34,8 @@ deroos = {'active':[
                   },
                 { 'scholar_name': 'ali gomaa',
                   'sex': 'm',
-                  'topic': 'iftaa',
-                  'title': 'magles el gomaa',
+                  'topic': 'Iftaa',
+                  'title': 'Magles el gomaa',
                   'location': 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.106349357143!2d30.979733965257697!3d29.976550381906968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458572a278e4c25%3A0x12ccf9e8eab22aec!2sFadel+Mosque%2C+Giza+Governorate%2C+Egypt!5e1!3m2!1sen!2sus!4v1521598451261',
                   'schedule':
                                 {'weekly_or_monthly': 'm',
@@ -63,6 +63,7 @@ deroos = {'active':[
                   {'scholar_name': 'amr elwerdany',
                   'sex': 'm',
                   'topic': 'fiqh',
+                  'title': 'ketab 2bn 3ata2 allah',
                   'location': 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.106349357143!2d30.979733965257697!3d29.976550381906968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458572a278e4c25%3A0x12ccf9e8eab22aec!2sFadel+Mosque%2C+Giza+Governorate%2C+Egypt!5e1!3m2!1sen!2sus!4v1521598451261',
                   'schedule':
                                 {'weekly_or_monthly': 'm',
